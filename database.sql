2cdbfba5f6036dcc261346bbe224da7ed077ac54
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-02 14:31:29 +0700

    first commit

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE belajar_laravel_database;
+
+use balajar_laravel_database;
+
+-- CREATE TABLE categories {
+--     id          VARCHAR(100) not NULL PRIMARY KEY,
+--     name        VARCHAR(100) not NULL,
+--     description TEXT,
+--     created_at  TIMESTAMP
+-- } engine innodb;
