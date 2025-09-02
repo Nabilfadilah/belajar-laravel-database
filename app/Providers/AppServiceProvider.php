0943e7283c78b35c02ac886af41b257be38de47b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-02 14:37:48 +0700

    Debug query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // untuk melakukan debug
+        // ketika pertaman kali aplikasi di jalankan
+        DB::listen(function (QueryExecuted $query) {
+            Log::info($query->sql);
+        });
     }
 }
